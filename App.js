7b686bc47d5550e7bb3c593b060120f88fdce859
mattg95 <mattgoodman95@gmail.com>
2021-03-23 13:40:40 +0000

    initial commit

--- App.js
+++ App.js
@@ -16,79 +16,101 @@ import {
   StatusBar,
   Button,
   Image,
+  Icon,
 } from 'react-native';
+
+import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
+import {faCoffee} from '@fortawesome/free-solid-svg-icons';
 
 import data from './data.js';
 
-const getRandomCard = (input) => {
-  return Math.floor(Math.random() * input.length);
-};
-
 const App: () => React$Node = () => {
   const [index, setIndex] = useState(0);
-  console.log(data[index]);
+
+  const getRandomCard = (input) => {
+    let newNum = Math.floor(Math.random() * (input.length - 1) + 1);
+    console.log(newNum);
+    if (newNum !== index) {
+      setIndex(newNum);
+      return;
+    } else return getRandomCard(input);
+  };
+
   return (
     <>
       <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>{data[index].title}</Text>
-              <Image source={data[index].image} style={styles.images} />
-              <Button
-                title="New Card"
-                onPress={() => setIndex(getRandomCard(data))}
-              />
-              <Text style={styles.sectionTitle}>{data[index].description}</Text>
-              <Text style={styles.bottomTitle}>{data[index].title}</Text>
-            </View>
+      <SafeAreaView style={styles.safeAreaView}>
+        <View style={styles.body}>
+          <View style={styles.sectionContainer}>
+            <Text style={styles.sectionTitle}>{data[index].title}</Text>
+            <Image source={data[index].image} style={styles.images} />
+            <FontAwesomeIcon icon={faCoffee} />
+
+            <Button
+              title="New Card"
+              style={styles.button}
+              onPress={() => getRandomCard(data)}
+            />
+            <Text style={styles.sectionTitle}>{data[index].description}</Text>
+            <Text style={styles.bottomTitle}>{data[index].title}</Text>
           </View>
-        </ScrollView>
+        </View>
       </SafeAreaView>
     </>
   );
 };
 
 const styles = StyleSheet.create({
-  SafeAreaView: {
+  safeAreaView: {
     flex: 1,
+    display: 'flex',
+    backgroundColor: 'white',
+  },
+  engine: {
+    position: 'absolute',
+    right: 0,
   },
   body: {
-    backgroundColor: 'white',
     flex: 1,
   },
   sectionContainer: {
     flex: 1,
-    height: '100%',
     marginTop: 32,
     paddingHorizontal: 24,
+    display: 'flex',
+    alignItems: 'center',
   },
   sectionTitle: {
-    fontSize: 24,
+    flex: 1,
+    fontSize: 32,
     fontWeight: '600',
+    width: '100%',
     color: 'black',
   },
   bottomTitle: {
-    fontSize: 24,
+    flex: 1,
+    fontSize: 32,
     fontWeight: '600',
     color: 'black',
+    width: '100%',
     textAlign: 'left',
     transform: [{rotateX: '180deg'}],
     transform: [{rotateY: '180deg'}],
   },
   images: {
-    width: 50,
+    flex: 3,
     height: 50,
-    resizeMode: 'stretch',
+    width: '100%',
+    resizeMode: 'cover',
+  },
+  buton: {
+    flex: 1,
   },
   sectionDescription: {
     marginTop: 8,
     fontSize: 18,
     fontWeight: '400',
-    color: 'grey',
+    color: 'black',
   },
   highlight: {
     fontWeight: '700',
